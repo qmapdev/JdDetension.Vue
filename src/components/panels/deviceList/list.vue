da2a3b33a75dfd6f69aea148106f281b762706ac
LiuPing1993 <39784291+LiuPing1993@users.noreply.github.com>
2020-06-28 15:12:32 +0800

    设备列表获取

--- src/components/panels/deviceList/list.vue
+++ src/components/panels/deviceList/list.vue
@@ -12,6 +12,120 @@
       </div>
       <div class="box-sblbdiv0">
         <div class="sblb-ttabbox">
+          <div class="sblb-ttab sblb-ttab-left" value="entrance" @click="initData('entrance')">
+            <div class="sblb-ttab-icon"></div>
+            <div class="sblb-ttab-name">门禁</div>
+            <div class="sblb-ttab-pic"></div>
+          </div>
+          <div class="sblb-ttab sblb-ttab-center" value="camera" @click="initData('camera')">
+            <div class="sblb-ttab-icon"></div>
+            <div class="sblb-ttab-name">摄像头</div>
+            <div class="sblb-ttab-pic"></div>
+          </div>
+          <div class="sblb-ttab sblb-ttab-right" value="call" @click="initData('call')">
+            <div class="sblb-ttab-icon"></div>
+            <div class="sblb-ttab-name">对讲机</div>
+            <div class="sblb-ttab-pic"></div>
+          </div>
+        </div>
+      </div>
+      <div class="box-sblbdiv1 flex">
+        <div class="select-parent sblb-select1">
+          <button class="select-value">建筑</button>
+          <input
+            type="hidden"
+            class="hidden"
+            value="462ae5a9-f778-4c66-b4c9-d4aca1fd0a92"
+            id="BuildingName_hidden"
+          />
+          <ul class="select-listul building-ul scrolldiv"></ul>
+          <div class="select-ieicon ieicon-1"></div>
+        </div>
+        <div class="select-parent sblb-select2">
+          <button class="select-value">楼层</button>
+          <input type="hidden" class="hidden" value="楼层" id="Floor_hidden" />
+          <ul class="select-listul floor-ul scrolldiv" style="overflow-x:hidden;">
+            <!--<li value="">楼层</li>
+            <li value="1">楼层1</li>-->
+          </ul>
+          <div class="select-ieicon ieicon-1"></div>
+        </div>
+        <div class="select-parent sblb-select3">
+          <button class="select-value">名称</button>
+          <ul class="select-listul">
+            <li>名称</li>
+            <li>名称1</li>
+          </ul>
+          <div class="select-ieicon ieicon-1"></div>
+        </div>
+        <!--<button class="search-btn"><img src="Content/images/icon-5.png" /></button>-->
+        <button class="search-btn" @click="initData(datatype);">
+          <div></div>
+        </button>
+      </div>
+      <div class="box-sblbdiv2">
+        <div class="box-listbox">
+          <ul class="box-list scrolldiv" id="device_ul">
+            <li class="list flex active" :key="item.camera_play.IP" v-for="item in deviceList" buildId="462ae5a9-f778-4c66-b4c9-d4aca1fd0a92" :Floor="3" :drawingno="item.camera_play.drawingno">
+                        <div class="list-div flex">
+                            <div class="list-symbol"></div>
+                            <div class="list-text">
+                                <div class="list-name">{{item.name}}<span>{{(item.status == null || item.status == "")?"异常":item.status}}</span></div>
+                                <div class="list-pos">{{'看守所'+item.builfloor + '层' + item.areaname}}</div>
+                            </div>
+                        </div>
+                    </li>
+                    <!--<li class="list flex ">
+                        <div class="list-div flex">
+                            <div class="list-symbol"></div>
+                            <div class="list-text">
+                                <div class="list-name">摄像头001 <span>离线</span></div>
+                                <div class="list-pos">行政楼二楼1032室门口</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list flex online">
+                        <div class="list-div flex">
+                            <div class="list-symbol"></div>
+                            <div class="list-text">
+                                <div class="list-name">摄像头001 <span>在线</span></div>
+                                <div class="list-pos">行政楼二楼1032室门口</div>
+                            </div>
+                        </div>
+            </li>-->
+          </ul>
+        </div>
+        <div class="pagination-box">
+          <div class="pagination" id="div-pagisnation">
+            <span class="current prev">上一页</span>
+            <span class="current">1</span>
+            <a style="cursor:pointer;">2</a>
+            <a style="cursor:pointer;">...</a>
+            <a style="cursor:pointer;">4</a>
+            <a style="cursor:pointer;" class="next">下一页</a>
+          </div>
+        </div>
+        <div class="sblb-btnbox flex">
+          <button>在线设备</button>
+          <button>离线设备</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+  <!--  ****************************************兴趣点列表************************************* -->
+  <div class="box-wrap">
+    <span class="box-anglel"></span>
+    <span class="box-angler"></span>
+    <div class="box box-sblb">
+      <div class="box-top">
+        <div class="box-title">设备列表</div>
+        <button class="box-close" onclick="javascript:$('#left_01').html('');">
+          <div></div>
+        </button>
+      </div>
+      <div class="box-sblbdiv0">
+        <div class="sblb-ttabbox">
           <div class="sblb-ttab sblb-ttab-left" value="entrance">
             <div class="sblb-ttab-icon"></div>
             <div class="sblb-ttab-name">门禁</div>
@@ -121,23 +235,27 @@ export default {
   name: "list",
   data() {
     return {
-      points: []
+      datatype:"",
+      floor:0,
+      BuildFloorId:"",
+      deviceList: [],
     };
   },
   computed: {
   },
   mounted() {
-    this.initData();
+    this.initData("camera");
   },
   methods: {
     /**
      * 通过axios直接获取数据  
      */   
-    async initData() {
+    async initData(devicetype) {
+       this.datatype=devicetype;
       this.points = [];
       let result = await getDeviceInfoList();
-      console.log(result);
-      // this.points = [...result]; // 如果使用箭头函数，this.points定义undefined错误。
+      console.log(result.data);
+       this.deviceList = result.data;//[...result];  // 如果使用箭头函数，this.points定义undefined错误。
     }
     
     /**
